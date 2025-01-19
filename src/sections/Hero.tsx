4ecff935cf2112c043905acc1e366391b839f7db
RISHI RAWAT <sl177y.log0@gmail.com>
2025-01-19 12:33:20 +0530

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -18,7 +18,7 @@ const Hero: FC = () => {
   const portraitWidth = useTransform(scrollYProgress, [0, 1], ["100%", "240%"]);
 
   const checkScreenSize = () => {
-    if (window.innerWidth <  500 || window.innerHeight < 766) {
+    if (window.innerWidth <  757 || window.innerHeight < 766) {
       setIsSmallScreen(true);
     } else {
       setIsSmallScreen(false);
